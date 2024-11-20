ba477bd8af4ed3a2d7f823777fb6bc59f29b6b0e
yangyyyyyyyyyy <22819690@qq.com>
2024-11-20 11:42:13 +0800

    将模型可以移动到cuda上，同时encode可以自动截断文本

--- module/embeddings.py
+++ module/embeddings.py
@@ -3,11 +3,11 @@ from sentence_transformers import SentenceTransformer
 class Emebeddings:
 
     def __init__(self,model_name) -> None:
-        self.model = SentenceTransformer(model_name,trust_remote_code=True,device='cpu')
+        self.model = SentenceTransformer(model_name,trust_remote_code=True,device='cuda')
         
 
     def encode(self, texts):
-        return self.model.encode(texts,batch_size=32,convert_to_numpy=True,show_progress_bar=True)
+        return self.model.encode(texts,batch_size=32,convert_to_numpy=True,show_progress_bar=True,truncate=True)
     
     def __repr__(self) -> str:
         return str(self.model)
